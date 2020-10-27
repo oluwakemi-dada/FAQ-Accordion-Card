79a0ddbeea178002db4d3f7d3c2c4d7bd83ffb2c
Dada Oluwakemi <dadaoluwakemi97@gmail.com>
2020-10-26 21:41:27 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const accordions = document.querySelectorAll('#accordion');
+const accordionArr = Array.from(accordion);
+const accordionCard = document.querySelector('#accordion-card');
+const imgDiv = document.querySelector('.desktop-index-three');
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  imgDiv.classList.remove('active')
+})
+
+accordionArr.forEach((accordion) => {
+  accordion.addEventListener('click', () => {
+    accordion.classList.toggle('active')
+  })
+})
+
+accordionCard.addEventListener('mouseover', (e) => {
+  const target = e.target.parentNode.parentNode.previousElementSibling.parentNode.previousElementSibling.children[2];
+
+  target.classList.add('active')
+  // console.log(target);
+  // console.log(e.type);
+})
+
+accordionCard.addEventListener('mouseout', (e) => {
+  const target = e.target.parentNode.parentNode.previousElementSibling.parentNode.previousElementSibling.children[2];
+
+  target.classList.remove('active')
+  // console.log(target);
+  // console.log(e.type);
+})
+
