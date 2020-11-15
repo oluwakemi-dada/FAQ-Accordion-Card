18195287bad0834eda9b79dec3ad1b460132ca21
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2020-11-15 17:26:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,29 +3,30 @@ const accordionArr = Array.from(accordions);
 const accordionCard = document.querySelector('#accordion-card');
 const imgDiv = document.querySelector('.desktop-index-three');
 
-window.addEventListener('DOMContentLoaded', (e) => {
-  imgDiv.classList.remove('active')
-})
+imgDiv.classList.remove('active');
 
 accordionArr.forEach((accordion) => {
   accordion.addEventListener('click', () => {
-    accordion.classList.toggle('active')
-  })
-})
+    accordion.classList.toggle('active');
+  });
+});
 
 accordionCard.addEventListener('mouseover', (e) => {
-  const target = e.target.parentNode.parentNode.previousElementSibling.parentNode.previousElementSibling.children[2];
+  const target =
+    e.target.parentNode.parentNode.previousElementSibling.parentNode
+      .previousElementSibling.children[1];
 
-  target.classList.add('active')
+  target.classList.add('active');
   // console.log(target);
   // console.log(e.type);
-})
+});
 
 accordionCard.addEventListener('mouseout', (e) => {
-  const target = e.target.parentNode.parentNode.previousElementSibling.parentNode.previousElementSibling.children[2];
+  const target =
+    e.target.parentNode.parentNode.previousElementSibling.parentNode
+      .previousElementSibling.children[1];
 
-  target.classList.remove('active')
+  target.classList.remove('active');
   // console.log(target);
   // console.log(e.type);
-})
-
+});
